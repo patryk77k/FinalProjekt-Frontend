208b1b3eaa36dbb0cce892661dec90418df51630
patryk77k <patryk77.k@gmail.com>
2023-02-21 23:31:16 +0100

    workers gefetcht

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
+import { useEffect, useState } from "react";
+
 function App() {
+  const [backend, setBackend] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:8080/workers")
+      .then((res) => res.json())
+      .then((data) => setBackend(data))
+      .catch((err) => console.log(err));
+  }, []);
+
   return (
     <div className="App">
-      <p>Hello World</p>
+      {backend.length
+        ? backend.map((simpleWorker) => <p>{simpleWorker.first_name}</p>)
+        : "...loading"}
     </div>
   );
 }
