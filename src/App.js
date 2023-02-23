a37b1b7c95c78f3c2b70b2ab75d368c470fc94e2
patryk77k <patryk77.k@gmail.com>
2023-02-23 13:08:31 +0100

    Routes erstellt

--- src/App.js
+++ src/App.js
@@ -1,4 +1,12 @@
 import { useEffect, useState } from "react";
+import { NavLink, Routes, Route } from "react-router-dom";
+import Home from "./components/Home";
+import AboutUs from "./components/AboutUs";
+import Contact from "./components/Contact";
+import Date from "./components/Date";
+import Login from "./components/Login";
+import Workers from "./components/Wokers";
+import WorkerDetails from "./components/WorkerDetails";
 
 function App() {
   const [backend, setBackend] = useState([]);
@@ -15,6 +23,17 @@ function App() {
       {/* {backend.length
         ? backend.map((simpleWorker) => <p>{simpleWorker.first_name}</p>)
         : "...loading"} */}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/workers" element={<Workers />} />
+        <Route path="/workers/:id" element={<WorkerDetails />} />
+        <Route path="/workers/:id/date" element={<Date />} />
+        <Route path="/about-us" element={<AboutUs />} />
+        <Route path="/contact" element={<Contact />} />
+
+        <Route />
+      </Routes>
     </div>
   );
 }
